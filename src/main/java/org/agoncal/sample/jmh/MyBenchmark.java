b373be8af7085a927793ca321d186b21fd98794a
kobe19881208 <kobe19881208@163.com>
2019-01-29 23:01:18 +0800

    [qh] add logging benchmark

--- src/main/java/org/agoncal/sample/jmh/MyBenchmark.java
+++ src/main/java/org/agoncal/sample/jmh/MyBenchmark.java
@@ -31,14 +31,60 @@
 
 package org.agoncal.sample.jmh;
 
-import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Thread)
 public class MyBenchmark {
 
-    @Benchmark
-    public void testMethod() {
-        // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
-        // Put your benchmark code here.
+    private static final Logger logger = LoggerFactory.getLogger(MyBenchmark.class);
+
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(Benchmark.class.getSimpleName())
+                .forks(1)
+                .warmupIterations(3)
+                .measurementIterations(3)
+                .build();
+        new Runner(opt).run();
     }
 
+    @Benchmark
+    public void testConcatenatingStrings() {
+        String x = "", y = "", z = "";
+        for (int i = 0; i < 100; i++) {
+            x += i; y += i; z += i;
+            logger.debug("Concatenating strings " + x + y + z);
+        }
+    }
+
+    @Benchmark
+    public void testVariableArguments() {
+        String x = "", y = "", z = "";
+        for (int i = 0; i < 100; i++) {
+            x += i; y += i; z += i;
+            logger.debug("Variable arguments {} {} {}", x, y, z);
+        }
+    }
+
+    @Benchmark
+    public void testIfDebugEnabled() {
+        String x = "", y = "", z = "";
+        for (int i = 0; i < 100; i++) {
+            x += i; y += i; z += i;
+            if (logger.isDebugEnabled()) {
+                logger.debug("If debug enabled {} {} {}", x, y, z);
+            }
+        }
+    }
 }
